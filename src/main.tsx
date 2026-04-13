4730b09486737eeed2295c1a9ec00390c69a5c2d
Md Fuadul Islam Redoy <mdfuadulislamredoy@Mds-MacBook-Air.local>
2026-04-13 18:47:42 +0600

    [FIX]: AWS Build issues fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store.ts";
+import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import Spinner from "./components/shared/Spinner";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
